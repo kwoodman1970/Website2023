8bf44ce9a1e2c663320b470db3740da3c9a8af29
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-01-06 02:09:41 -0700

    Fixed formatting of README.md on Portfolio page and improved handling of fetch errors

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -5,26 +5,70 @@ import remarkGfm from "remark-gfm";
 
 import "./portfolio.css";
 
-const URLRoot = "https://raw.githubusercontent.com/kwoodman1970/kwoodman1970/main/";
+const URLRoot   = "https://raw.githubusercontent.com/kwoodman1970/kwoodman1970/main/";
 const readmeURL = URLRoot + "README.md";
 
 function PortfolioPage()
 {
-  const {isLoading, data, error} = useFetch(readmeURL, {formatter:  (res) => res.text()});
+  /*
+  There's a defect in the "useFetch" component:  if a formatter is specified and there's an
+  error then the "error" constant isn't populated.  A special error-trapping formatter function
+  is therefore required to properly detect a failed fetch operation.
+
+  If that defect is ever corrected then the special formatter function will become deprecated.
+  */
+
+  function formatter(response)
+  {
+    if (!response.ok)
+    {
+      const error = new Error("Fetch error");
+
+      error.status     = response.status;
+      error.statusText = response.statusText;
+
+      throw error;
+    }
+
+    return response.text()
+  }
+
+  // const {isLoading, data, error} = useFetch(readmeURL, {formatter:  (res) => res.text()});
+  const {isLoading, data, error} = useFetch(readmeURL, {formatter:  formatter});
 
   if (isLoading)
-    return (<Fragment><main>Loading...</main></Fragment>)
+    return (
+      <Fragment>
+        <main>
+          <h2>My Online Digital Portfolio</h2>
+
+          <p>Fetching portfolio &ndash; please stand by...</p>
+        </main>
+      </Fragment>)
   else if (error)
   {
     console.log(`Failed to fetch URL "${readmeURL}"`);
-    console.log(error);
+    console.log(`Response status = ${error.status}:  "${error.statusText}"`);
 
     return (
       <Fragment>
         <main>
-          <p>Unable to read portfolio file.</p>
-          <p>Result code:  {error.status}</p>
-          <p>Result status:  {error.statusText}</p>
+          <h2>My Online Digital Portfolio</h2>
+
+          <p>Unable to fetch portfolio.</p>
+          <p>
+            Response status:  <b>{error.status}</b><br />
+            Response message:  <b>{error.statusText}</b>
+          </p>
+
+          <hr />
+
+          <p>
+            Well, <i>that</i> wasn&apos;t supposed to happen.  Please accept my apologies.
+            You can have a look at
+            {" "}<a href="https://github.com/kwoodman1970">my GitHub page</a> while I don my
+            powered armour and go bug-hunting.
+          </p>
         </main>
       </Fragment>)
   }
@@ -32,26 +76,24 @@ function PortfolioPage()
   {
     let markdownText = data.replace("<img src=\".README/banner.jpg\" alt=\"Banner\">", "");
 
-    markdownText = markdownText.replace("###### ", "");
-    markdownText = markdownText.replace("##### ", "###### ");
-    markdownText = markdownText.replace("#### ", "##### ");
-    markdownText = markdownText.replace("### ", "#### ");
-    markdownText = markdownText.replace("## ", "### ");
-    markdownText = markdownText.replace("# ", "## ");
+    markdownText = markdownText.replaceAll("###### ", "");
+    markdownText = markdownText.replaceAll("# ", "## ");
+
+    markdownText = markdownText.replaceAll("<br />", "\n");
 
     return (
       <Fragment>
         <main>
+          <section id="Portfolio">
+            <Markdown remarkPlugins={[remarkGfm]}>{markdownText}</Markdown>
+          </section>
+
+          <hr />
+
           <p>
             <b>Fun Fact:</b> This website is one of the projects in my online
             portfolio.  <i>How meta is that?</i>
           </p>
-
-          <hr />
-
-          <section id="Portfolio">
-            <Markdown remarkPlugins={[remarkGfm]}>{markdownText}</Markdown>
-          </section>
         </main>
       </Fragment>
     );
