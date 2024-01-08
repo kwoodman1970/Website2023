c8dee5e5ef45783b8bdbb88be3203225fdd74bd4
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-01-08 01:31:57 -0700

    Added subheadings to Contact page and fixed/cleaned up the layout CSS

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -34,6 +34,7 @@ function ContactPage()
     <Fragment>
       <main>
         <h2>How to Contact Me</h2>
+
         <p>
           The best way to contact me is by e-mail:{"  "}
           <a id="hiddenlink" href="" onClick={setHref}>
@@ -43,51 +44,61 @@ function ContactPage()
 
         <hr />
 
-        <section style={{display:  "inline-block"}}>
-          <p>
-            You can use this form to quickly send me a message through your own e-mail
-            program:
-          </p>
+        <div id="Methods">
+          <section>
+            <h3>Send Me An E-mail &ndash; Right Now</h3>
 
-          <form method="post" action="" encType="text/plain" onSubmit={setAction}>
-            <table>
-              <tbody>
-                <tr>
-                  <td>Your name is:</td>
-                  <td><input name="RealName" type="text" size="40" /></td>
-                </tr>
-                <tr>
-                  <td>Your message is:</td>
-                  <td><textarea name="Message" wrap="soft" rows="12" cols="40" /></td>
-                </tr>
-              </tbody>
-            </table>
+            <p>
+              You can use this form to quickly send me a message through your own e-mail
+              program:
+            </p>
 
-            <input type="submit" value="Send Now"/>
-            <input type="reset" value="Start Over" />
-          </form>
-        </section>
+            <form method="post" action="" encType="text/plain" onSubmit={setAction}>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Your name is:</td>
+                    <td><input name="RealName" type="text" size="40" /></td>
+                  </tr>
+                  <tr>
+                    <td>Your message is:</td>
+                    <td><textarea name="Message" wrap="soft" rows="12" cols="40" /></td>
+                  </tr>
+                </tbody>
+              </table>
 
-        <a className="QR" href="https://linkedin.com/in/woodmankevin/">
-          <figure>
-            <img src="linkedin_qr.svg" alt="LinkedIn QR Code" />
-            <figcaption style={{gridTemplateColumns:  "112px auto"}}>
-              <img src="LinkedIn_Logo.svg" alt="LinkedIn Profile" />
-              <p  style={{margin:  "auto 19px auto 0px", textAlign:  "center"}} >
-                View My LinkedIn<sup>&reg;</sup> Profile
-              </p>
-            </figcaption>
-          </figure>
-        </a>
+              <input type="submit" value="Send Now"/>
+              <input type="reset" value="Start Over" />
+            </form>
+          </section>
 
-        <a className="QR" href="https://github.com/kwoodman1970/">
-          <figure>
-            <img src="github_qr.svg" alt="GitHub QR Code" />
-            <figcaption style={{gridTemplateColumns:  "auto", }}>
-              <img src="GitHub_Logo.svg" alt="GitHub Portfolio" />
-            </figcaption>
-          </figure>
-        </a>
+          <section>
+            <h3>Additional Web Presence</h3>
+
+            <p id="Links">
+              <a href="https://linkedin.com/in/woodmankevin/">
+                <figure>
+                  <img src="linkedin_qr.svg" alt="LinkedIn QR Code" />
+                  <figcaption style={{display:  "grid", gridTemplateColumns:  "112px auto"}}>
+                    <img src="LinkedIn_Logo.svg" alt="LinkedIn Profile" />
+                    <p  style={{margin:  "auto 19px auto 0px", textAlign:  "center"}} >
+                      View My LinkedIn<sup>&reg;</sup> Profile
+                    </p>
+                  </figcaption>
+                </figure>
+              </a>
+
+              <a href="https://github.com/kwoodman1970/">
+                <figure>
+                  <img src="github_qr.svg" alt="GitHub QR Code" />
+                  <figcaption>
+                    <img src="GitHub_Logo.svg" alt="GitHub Portfolio" />
+                  </figcaption>
+                </figure>
+              </a>
+            </p>
+          </section>
+        </div>
       </main>
     </Fragment>)
 }
