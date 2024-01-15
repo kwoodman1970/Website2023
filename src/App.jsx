949b0a162c2bca2a5b5bb45ac31db1b6ea65a464
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-01-15 00:33:18 -0700

    Moved layout rules in App.css into index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-import "./App.css";
+// import "./App.css";
 
 import HomePage from "./pages/home";
 import PortfolioPage from "./pages/portfolio";
