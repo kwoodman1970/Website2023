7434545e6ad7bf6cb673041cbb79db09c38fe8aa
Kevin Woodman <kwoodman1970@yahoo.ca>
2026-05-18 23:18:07 -0600

    Customized the 404 page with options that depend on the referrer

--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -18,6 +18,10 @@ function NotFoundPage({ referrerURL }) {
   const githubIssueUrl = "https://github.com/kwoodman1970/Website2023/issues/new?title=" +
     issueTitle + "&body=" + issueBody;
 
+  const isFromThisSite = referrerURL.startsWith(window.location.origin);
+  const isFromExternalSite = ((referrerURL !== "") && !isFromThisSite);
+  const isDirectAccess = (referrerURL === "");
+
   return (
     <main>
       <h2>404 &ndash; Page Not Found</h2>
@@ -41,13 +45,33 @@ function NotFoundPage({ referrerURL }) {
         <li>
           Go directly to the <a href="/">home page</a> (do not pass Go; do not collect $200)
         </li>
-        <li>
-          If you got here via a page on this website then please{" "}
-          <a href={githubIssueUrl} target="_blank" rel="noopener noreferrer">report it</a> so
-          that I can fix it.  You can be sure that I&apos;ll be very embarrassed.
-        </li>
-      </ul>
 
+        {isFromThisSite && (
+          <li>
+            <strong>You got here from a link on this website.</strong>  Please{" "}
+            <a href={githubIssueUrl} target="_blank" rel="noopener noreferrer">report this
+            broken link</a> so that I can fix it.  You can be sure that I&apos;ll be very
+            embarrassed, but I&apos;d rather know than not know!
+          </li>
+        )}
+
+        {isFromExternalSite && (
+          <li>
+            <strong>You got here from <a href={referrerURL}><tt>{referrerURL}</tt></a>.</strong>  That&apos;s not within my
+            control.  You might want to let them know that they have a broken link so that they
+            can fix it.
+          </li>
+        )}
+
+        {isDirectAccess && (
+          <li>
+            If you typed this URL directly or used a bookmark then double-check for typos.  If
+            you&apos;re still certain that this page should exist then please{" "}
+            <a href={githubIssueUrl} target="_blank" rel="noopener noreferrer">let me
+            know</a> so that I can do something about it.
+          </li>
+        )}
+      </ul>
     </main>
   );
 }
